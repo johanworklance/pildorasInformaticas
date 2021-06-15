77786d84d1216dc343a28df1db4e42f254ec6c64
johan <johan.worklance@gmail.com>
2021-06-14 20:54:03 -0430

    act

--- CRUD1/index.php
+++ CRUD1/index.php
@@ -7,7 +7,17 @@
 
 
 </head>
-
+<style>
+    a{
+        padding:5px;
+        margin:0 5px;
+        text-decoration:none;
+        color:black;
+    }
+    a:hover{
+        background-color:#9ea0a3;
+    }
+</style>
 <body>
 <?php
     include_once('conexion.php');
@@ -15,7 +25,40 @@
     /* $conexion= $base->query("SELECT * FROM datos_usuarios");
     $registros= $conexion->fetchAll(PDO::FETCH_OBJ); */
 
-    $registros= $base->query("SELECT * FROM datos_usuarios")->fetchAll(PDO::FETCH_OBJ);//es un objeto no un array, a sus propiedades se accede con el comando ->
+    //-------------------------paginacion---------------------------------------
+    $tamañoPaginas=3;//numero de items por pagina, y segundo parametro del LIMIT que indica hasta cuantos registros traer
+
+    $pagina=1;//numero de la pagina actual
+
+    if(isset($_GET['pagina'])){
+        if($_GET['pagina']==1){
+
+            header("Location:index.php");
+        }else{
+            $pagina=$_GET['pagina'];//numero de la pagina actual
+        }
+    }
+
+    
+    $empezarDesde=($pagina-1)*$tamañoPaginas;//indice para el LIMIT de la sentencia, desde donde hara la consulta, si la pagina es 1 entonces seria 1-0=0 0*3=0 entonces comienza desde el registro 0
+
+    $sql="SELECT * FROM datos_usuarios";
+
+    $resultado=$base->prepare($sql);
+
+    $resultado->execute(array());
+
+    $numFilas=$resultado->rowCount();
+
+    $totalPaginas= ceil($numFilas/$tamañoPaginas);
+
+    $resultado->closeCursor();
+
+    //--------------------------------------------------------------------------
+
+
+
+    $registros= $base->query("SELECT * FROM datos_usuarios LIMIT $empezarDesde,$tamañoPaginas")->fetchAll(PDO::FETCH_OBJ);//es un objeto no un array, a sus propiedades se accede con el comando ->
     if(isset($_POST['cr'])){
 
       $nombre= $_POST['Nom'];
@@ -67,9 +110,21 @@
         <td><input type='text' name='Nom' size='10' class='centrado'></td>
         <td><input type='text' name='Ape' size='10' class='centrado'></td>
         <td><input type='text' name=' Dir' size='10' class='centrado'></td>
-        <td class='bot'><input type='submit' name='cr' id='cr' value='Insertar'></td></tr>    
+        <td class='bot'><input type='submit' name='cr' id='cr' value='Insertar'></td></tr> 
+        <tr>
+        <td colspan="4">
+            <?php
+            for($i=1;$i<=$totalPaginas;$i++){
+
+              echo "<a href='?pagina=$i'> $i </a>";//se pueden enviar numeros a la propia pagina con esa 'direccion'
+            }
+
+            ?>
+        </td>
+        </tr>   
     </table>
 </form>
+    
 <p>&nbsp;</p>
 </body>
 </html>
